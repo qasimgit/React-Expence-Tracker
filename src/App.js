fe80a89f4b12df2dcd7319568ffdfe6cb8bea81b
qasimgit <qasimpkkhi@gmail.com>
2020-06-26 17:46:19 +0500

    header component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from './components/header';
 
 function App() {
   return (
-    <div>
+    <div className='container'>
+      <Header />
 
     </div>
-    
+
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <div>
+            <h1>
+                Expence Tracker 
+            </h1>
+            <h4>By Muhammad Qasim Qadri</h4>
+        </div>
+    )
+}
